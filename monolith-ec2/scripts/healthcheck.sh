ca98710bced7c58ecc46a6a8820d0fd3c497f6f5
EC2 Default User <ec2-user@ip-10-100-11-45.ap-south-1.compute.internal>
2026-08-25 18:36:27 +0000

    health check sleep

--- monolith-ec2/scripts/healthcheck.sh
+++ monolith-ec2/scripts/healthcheck.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-curl -f http://localhost:8080/actuator/health
+
+for i in {1..12}; do
+    if curl -sf http://localhost:8080/actuator/health; then
+        echo "Application is healthy"
+        exit 0
+    fi
+
+    echo "Application not ready yet. Waiting 5 seconds..."
+    sleep 5
+done
+
+echo "Application failed health check"
+exit 1
